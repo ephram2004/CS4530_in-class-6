ec100c01d0e50bec459feada1c1a1d8f294ad88a
Ephram Cola Jacquin <colajacquin.e@northeastern.edu>
2025-07-01 17:26:32 +1000

    feat(db): add docker databasae setup

---
+++ REDatabase/init/01-schema.sql
@@ -0,0 +1,21 @@
+-- init/01-schema.sql
+
+CREATE TABLE property_sales (
+    property_id           SERIAL PRIMARY KEY,
+    download_date         DATE,
+    council_name          TEXT,
+    purchase_price        NUMERIC(12, 2),
+    address               TEXT,
+    post_code             VARCHAR(10),
+    property_type         TEXT,
+    strata_lot_number     TEXT,
+    property_name         TEXT,
+    area                  NUMERIC(10, 2),
+    area_type             TEXT,
+    contract_date         DATE,
+    settlement_date       DATE,
+    zoning                TEXT,
+    nature_of_property    TEXT,
+    primary_purpose       TEXT,
+    legal_description     TEXT
+);

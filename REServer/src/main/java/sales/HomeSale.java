4ba7c3974bdea3fbd0e98889e81515f1620ae9bf
Ephram Cola Jacquin <89238547+ephram2004@users.noreply.github.com>
2025-07-03 20:55:53 +1000

    In class 2 alex (#2)
    * added universal filter
    
    * xml
    
    * made HomeSale class fields private and added getters
    
    ---------
    
    Co-authored-by: ASun43 <40130467+alexsun2@users.noreply.github.com>

--- REServer/src/main/java/sales/HomeSale.java
+++ REServer/src/main/java/sales/HomeSale.java
@@ -5,11 +5,23 @@ import java.sql.Date;
 // "Simple" class to provide test data in SalesDAO
 
 public class HomeSale {
-    public String councilName, address, propertyType, propertyName, areaType, zoning,
-            natureOfProperty, primaryPurpose, legalDescription;
-    public int propertyId, purchasePrice, postCode, strataLotNumber;
-    public Date downloadDate, contractDate, settlementDate;
-    public double area;
+    private final String councilName;
+    private final String address;
+    private final String propertyType;
+    private final String propertyName;
+    private final String areaType;
+    private final String zoning;
+    private final String natureOfProperty;
+    private final String primaryPurpose;
+    private final String legalDescription;
+    private final int propertyId;
+    private final int purchasePrice;
+    private final int postCode;
+    private final int strataLotNumber;
+    private final Date downloadDate;
+    private final Date contractDate;
+    private final Date settlementDate;
+    private final double area;
 
     public HomeSale(int propertyId, Date downloadDate, String councilName,
             int purchasePrice, String address, int postCode,
@@ -33,5 +45,73 @@ public class HomeSale {
         this.natureOfProperty = natureOfProperty;
         this.primaryPurpose = primaryPurpose;
         this.legalDescription = legalDescription;
+    }
+
+    public int getPropertyId() {
+        return propertyId;
+    }
+
+    public Date getDownloadDate() {
+        return downloadDate;
+    }
+
+    public String getCouncilName() {
+        return councilName;
+    }
+
+    public int getPurchasePrice() {
+        return purchasePrice;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public int getPostCode() {
+        return postCode;
+    }
+
+    public String getPropertyType() {
+        return propertyType;
+    }
+
+    public int getStrataLotNumber() {
+        return strataLotNumber;
+    }
+
+    public String getPropertyName() {
+        return propertyName;
+    }
+
+    public double getArea() {
+        return area;
+    }
+
+    public String getAreaType() {
+        return areaType;
+    }
+
+    public Date getContractDate() {
+        return contractDate;
+    }
+
+    public Date getSettlementDate() {
+        return settlementDate;
+    }
+
+    public String getZoning() {
+        return zoning;
+    }
+
+    public String getNatureOfProperty() {
+        return natureOfProperty;
+    }
+
+    public String getPrimaryPurpose() {
+        return primaryPurpose;
+    }
+
+    public String getLegalDescription() {
+        return legalDescription;
     }
 }

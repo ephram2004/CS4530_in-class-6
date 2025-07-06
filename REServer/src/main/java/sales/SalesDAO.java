a10054391cd00ff563cd3d5a8785e64ccdae535f
Ephram Cola Jacquin <colajacquin.e@northeastern.edu>
2025-07-07 09:55:40 +1000

    fixes

--- REServer/src/main/java/sales/SalesDAO.java
+++ REServer/src/main/java/sales/SalesDAO.java
@@ -31,8 +31,11 @@ public class SalesDAO {
             System.out.println("Inserted new Sale.");
         } catch (Exception e) {
             System.err.println("Could not insert new sale! " + e);
+            // returns Optional wrapping a HomeSale if id is found, empty Optional otherwise
+        }
+        return true;
+    }
 
-    // returns Optional wrapping a HomeSale if id is found, empty Optional otherwise
     public Optional<HomeSale> getSaleById(int saleID) throws SQLException {
         String sql = "SELECT * FROM property_sales WHERE property_id = ?";
         HomeSale sale = null;
@@ -216,110 +219,5 @@ public class SalesDAO {
             }
         }
         return sales;
-    }
-
-    public int getPriceHistory(int propertyId) throws SQLException {
-        try (Connection conn = DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASSWORD);
-             PreparedStatement stmt = conn
-                     .prepareStatement(
-                             "SELECT\n" +
-                                     "  MAX(purchase_price) - MIN(purchase_price) AS price_change\n" +
-                                     "FROM property_sales\n" +
-                                     "WHERE property_id = ?")) {
-            stmt.setInt(1, propertyId);
-            ResultSet rs = stmt.executeQuery();
-            if (rs.next() && !rs.wasNull()) {
-                int priceChange = rs.getInt("price_change");
-                return priceChange;
-            } else {
-                return 0;
-            }
-        }
-    }
-
-    public List<HomeSale> filterSalesByCriteria(String councilName, String propertyType,
-            int minPrice, int maxPrice, String areaType) throws SQLException {
-        List<HomeSale> sales = new ArrayList<HomeSale>();
-        StringBuilder query = new StringBuilder("SELECT * FROM property_sales WHERE 1=1");
-        List<Object> params = new ArrayList<>();
-
-        if (councilName != null && !councilName.isEmpty()) {
-            query.append(" AND council_name = ?");
-            params.add(councilName);
-        }
-        if (propertyType != null && !propertyType.isEmpty()) {
-            query.append(" AND property_type = ?");
-            params.add(propertyType);
-        }
-        if (minPrice >= 0) {
-            query.append(" AND purchase_price >= ?");
-            params.add(minPrice);
-        }
-        if (maxPrice >= 0) {
-            query.append(" AND purchase_price <= ?");
-            params.add(maxPrice);
-        }
-        if (areaType != null && !areaType.isEmpty()) {
-            query.append(" AND area_type = ?");
-            params.add(areaType);
-        }
-
-        try (Connection conn = DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASSWORD);
-                PreparedStatement stmt = conn.prepareStatement(query.toString())) {
-            for (int i = 0; i < params.size(); i++) {
-                stmt.setObject(i + 1, params.get(i));
-            }
-            ResultSet rs = stmt.executeQuery();
-
-            while (rs.next()) {
-                HomeSale sale = new HomeSale(
-                        rs.getInt("property_id"),
-                        rs.getDate("download_date"),
-                        rs.getString("council_name"),
-                        rs.getInt("purchase_price"),
-                        rs.getString("address"),
-                        rs.getInt("post_code"),
-                        rs.getString("property_type"),
-                        rs.getInt("strata_lot_number"),
-                        rs.getString("property_name"),
-                        rs.getDouble("area"),
-                        rs.getString("area_type"),
-                        rs.getDate("contract_date"),
-                        rs.getDate("settlement_date"),
-                        rs.getString("zoning"),
-                        rs.getString("nature_of_property"),
-                        rs.getString("primary_purpose"),
-                        rs.getString("legal_description"));
-                sales.add(sale);
-            }
-        }
-        return sales;
-    }
-
-    public double getAveragePrice(int postCode) throws SQLException {   
-        //List<HomeSale> sales = new ArrayList<HomeSale>();
-        try (Connection conn = DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASSWORD);
-                PreparedStatement stmt = conn
-                        .prepareStatement(
-                                "SELECT AVG(purchase_price) AS average FROM property_sales WHERE post_code = ?")) {
-            stmt.setInt(1, postCode);
-            ResultSet rs = stmt.executeQuery();
-            System.out.println(rs);
-            if (rs.next() && !rs.wasNull()) {
-                double avg = rs.getDouble("average");
-                return round(avg, 2);
-            }
-            else {
-                return 0;
-            }
-        }
-    }
-
-    private static double round(double value, int places) {
-        if (places < 0) throw new IllegalArgumentException();
-
-        BigDecimal bd = BigDecimal.valueOf(value);
-        bd = bd.setScale(places, RoundingMode.HALF_UP);
-        return bd.doubleValue();
     }
 }

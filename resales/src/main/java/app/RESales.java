9e3464f44817bb4e690146ba20f531731b7332a2
ASun43 <40130467+alexsun2@users.noreply.github.com>
2025-07-09 11:03:38 +1000

    separated resales and removed metrics endpoints

---
+++ resales/src/main/java/app/RESales.java
@@ -0,0 +1,100 @@
+package app;
+
+import helpers.HelperSQL;
+import io.javalin.Javalin;
+import static io.javalin.apibuilder.ApiBuilder.get;
+import static io.javalin.apibuilder.ApiBuilder.path;
+import static io.javalin.apibuilder.ApiBuilder.post;
+import io.javalin.openapi.plugin.OpenApiPlugin;
+import io.javalin.openapi.plugin.redoc.ReDocPlugin;
+import io.javalin.openapi.plugin.swagger.SwaggerPlugin;
+import sql.sales.DynamicHomeSale;
+import sql.sales.SalesController;
+import sql.sales.SalesDAO;
+
+public class RESales {
+
+    public static void main(String[] args) {
+        // exporting schema to JSON file
+        HelperSQL.exportSchemaToFile(DynamicHomeSale.class);
+
+        // in memory test data store
+        var sales = new SalesDAO();
+
+        // API implementation
+        SalesController salesHandler = new SalesController(sales);
+
+        Javalin.create(config -> {
+            // OpenAPI doc plugins
+            config.registerPlugin(new OpenApiPlugin(pluginConfig -> {
+                pluginConfig.withDefinitionConfiguration((version, definition) -> {
+                    definition.withOpenApiInfo(info -> {
+                        info.setTitle("Real Estate API");
+                        info.setVersion("1.0.0");
+                        info.setDescription("API for querying property sales");
+                    });
+                });
+            }));
+            config.registerPlugin(new SwaggerPlugin());
+            config.registerPlugin(new ReDocPlugin());
+
+            // Route builder
+            config.router.apiBuilder(() -> {
+                // Health check
+                get("/", ctx -> ctx.result("Real Estate server is running"));
+                // Sales routes
+                path("sales", () -> {
+                    get(ctx -> {
+                        String councilName = ctx.queryParam("councilname");
+                        String propertyType = ctx.queryParam("propertytype");
+                        String areaType = ctx.queryParam("areatype");
+                        int minPrice = ctx.queryParam("minprice") != null ? Integer.parseInt(ctx.queryParam("minprice"))
+                                : -1;
+                        int maxPrice = ctx.queryParam("maxprice") != null ? Integer.parseInt(ctx.queryParam("maxprice"))
+                                : -1;
+
+                        boolean hasFilter = councilName != null || propertyType != null || areaType != null
+                                || minPrice >= 0 || maxPrice >= 0;
+
+                        if (hasFilter) {
+                            salesHandler.filterSalesByCriteria(ctx, councilName, propertyType, minPrice, maxPrice,
+                                    areaType);
+                        } else {
+                            salesHandler.getAllSales(ctx);
+                        }
+                    });
+
+                    post(salesHandler::createSale);
+
+                    path("postcode/{postcode}", () -> {
+                        get(ctx -> {
+                            salesHandler.findSaleByPostCode(ctx, Integer.parseInt(ctx.pathParam("postcode")));
+                        });
+                    });
+
+                    path("price-history/propertyId/{propertyID}", () -> get(ctx -> salesHandler
+                            .findPriceHistoryByPropertyId(ctx, Integer.parseInt(ctx.pathParam("propertyID")))));
+
+                    path("average/{postcode}", () -> get(
+                            ctx -> salesHandler.averagePrice(ctx, Integer.parseInt(ctx.pathParam("postcode")))));
+
+                    path("{saleID}", () -> {
+                        get(ctx -> {
+                            salesHandler.getSaleByID(ctx, Integer.parseInt(ctx.pathParam("saleID")));
+                        });
+                    });
+                });
+            });
+        }).start(7071);
+
+        // Console output
+        System.out.println(
+                "✅ Javalin server started at http://localhost:7071");
+        System.out.println(
+                "📘 Swagger UI: http://localhost:7071/swagger");
+        System.out.println(
+                "📕 ReDoc UI:   http://localhost:7071/redoc");
+        System.out.println(
+                "📘 OpenAPI:   http://localhost:7071/openapi");
+    }
+}
---
+++ resales/src/main/java/sql/sales/SalesController.java
@@ -0,0 +1,200 @@
+package sql.sales;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.javalin.http.Context;
+import io.javalin.openapi.HttpMethod;
+import io.javalin.openapi.OpenApi;
+import io.javalin.openapi.OpenApiContent;
+import io.javalin.openapi.OpenApiParam;
+import io.javalin.openapi.OpenApiRequestBody;
+import io.javalin.openapi.OpenApiResponse;
+
+public class SalesController {
+
+    private final SalesDAO homeSales;
+
+    public SalesController(SalesDAO homeSales) {
+        this.homeSales = homeSales;
+    }
+
+    @OpenApi(summary = "Create a new home sale", operationId = "createSale", path = "/sales", methods = HttpMethod.POST, tags = {
+        "Sales"}, requestBody = @OpenApiRequestBody(content = {
+        @OpenApiContent(from = DynamicHomeSale.class)}), responses = {
+        @OpenApiResponse(status = "201"),
+        @OpenApiResponse(status = "400")
+    })
+    // implements POST /sales
+    public void createSale(Context ctx) {
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        /*try {
+            JsonNode jsonSalesArray = mapper.readTree(ctx.body());
+            List<DynamicHomeSale> salesList = new ArrayList<>();
+            for (JsonNode sale : jsonSalesArray) {
+                DynamicHomeSale homeSale = new DynamicHomeSale(sale);
+                salesList.add(homeSale);
+            }
+        } catch (Exception e) {
+            ctx.status(400);
+            return;
+        }
+         */
+        // Extract Home Sale from request body
+        // TO DO override Validator exception method to report better error message
+        try {
+            JsonNode jsonSale = mapper.readTree(ctx.body()); // store new sale in data set
+            DynamicHomeSale sale = new DynamicHomeSale(jsonSale);
+            homeSales.newSale(sale);
+            ctx.result("Sale Created");
+            ctx.status(201);
+        } catch (Exception e) {
+            ctx.result("Failed to add sale: " + e.getMessage());
+            ctx.status(400);
+        }
+    }
+
+    @OpenApi(summary = "Get all sales", operationId = "getAllSales", path = "/sales", methods = HttpMethod.GET, tags = {
+        "Sales"}, responses = {
+        @OpenApiResponse(status = "200", content = {
+            @OpenApiContent(from = DynamicHomeSale[].class)}),
+        @OpenApiResponse(status = "404")
+    })
+    // implements Get /sales
+    public void getAllSales(Context ctx) {
+        try {
+            List<DynamicHomeSale> allSales = homeSales.getAllSales();
+            if (allSales.isEmpty()) {
+                ctx.result("No Sales Found");
+                ctx.status(404);
+            } else {
+                ctx.json(allSales);
+                ctx.status(200);
+            }
+        } catch (SQLException e) {
+            ctx.result("Error retrieving sale: " + e.getMessage());
+            ctx.status(400);
+        }
+    }
+
+    @OpenApi(summary = "Get sale by ID", operationId = "getSaleById", path = "/sales/{saleID}", methods = HttpMethod.GET, tags = {
+        "Sales"}, pathParams = {
+        @OpenApiParam(name = "saleID")
+    }, responses = {
+        @OpenApiResponse(status = "200", content = {
+            @OpenApiContent(from = DynamicHomeSale.class)}),
+        @OpenApiResponse(status = "404")
+    })
+    // implements GET /sales/{saleID}
+    public void getSaleByID(Context ctx, int id) {
+        try {
+            Optional<DynamicHomeSale> sale = homeSales.getSaleById(id);
+            sale.map(ctx::json).orElseGet(() -> {
+                ctx.result("Sale not found");
+                ctx.status(404);
+                return ctx;
+            });
+        } catch (SQLException e) {
+            ctx.result("Error retrieving sale: " + e.getMessage());
+            ctx.status(400);
+        }
+    }
+
+    @OpenApi(summary = "Get sales by postcode", operationId = "getSalesByPostcode", path = "/sales/postcode/{postcode}", methods = HttpMethod.GET, tags = {
+        "Sales"}, pathParams = {
+        @OpenApiParam(name = "postcode")
+    }, responses = {
+        @OpenApiResponse(status = "200", content = {
+            @OpenApiContent(from = DynamicHomeSale[].class)}),
+        @OpenApiResponse(status = "404")
+    })
+    // Implements GET /sales/postcode/{postcodeID}
+    public void findSaleByPostCode(Context ctx, int postCode) {
+        try {
+            List<DynamicHomeSale> sales = homeSales.getSalesByPostCode(postCode);
+            if (sales.isEmpty()) {
+                ctx.result("No sales for postcode found");
+                ctx.status(404);
+            } else {
+                ctx.json(sales);
+                ctx.status(200);
+            }
+        } catch (SQLException e) {
+            ctx.result("Error retrieving sale: " + e.getMessage());
+            ctx.status(400);
+        }
+    }
+
+    @OpenApi(summary = "Get price history (diff) by property ID", operationId = "getPriceHistory", path = "/sales/propertyId/{propertyID}", methods = HttpMethod.GET, tags = {
+        "Sales"}, pathParams = {
+        @OpenApiParam(name = "propertyID")
+    }, responses = {
+        @OpenApiResponse(status = "200"),
+        @OpenApiResponse(status = "400")
+    })
+    public void findPriceHistoryByPropertyId(Context ctx, int propertyId) {
+        try {
+            int priceDiff = homeSales.getPriceHistory(propertyId);
+            ctx.result(String.valueOf(priceDiff));
+            ctx.status(200);
+        } catch (SQLException e) {
+            ctx.result("Error retrieving price difference: " + e.getMessage());
+        }
+    }
+
+    @OpenApi(summary = "Filter sales by criteria", operationId = "filterSales", path = "/sales", // If you're keeping
+            // filters as query
+            // params on /sales
+            methods = HttpMethod.GET, tags = {"Sales"}, queryParams = {
+                @OpenApiParam(name = "councilname", required = false),
+                @OpenApiParam(name = "propertytype", required = false),
+                @OpenApiParam(name = "areatype", required = false),
+                @OpenApiParam(name = "minprice", required = false),
+                @OpenApiParam(name = "maxprice", required = false)
+            }, responses = {
+                @OpenApiResponse(status = "200", content = {
+            @OpenApiContent(from = DynamicHomeSale[].class)}),
+                @OpenApiResponse(status = "404")
+            })
+    public void filterSalesByCriteria(Context ctx, String councilName, String propertyType, int minPrice, int maxPrice,
+            String areaType) {
+        try {
+            List<DynamicHomeSale> sales = homeSales.filterSalesByCriteria(councilName, propertyType, minPrice, maxPrice,
+                    areaType);
+
+            if (sales.isEmpty()) {
+                ctx.result("No sales found");
+                ctx.status(404);
+            } else {
+                ctx.json(sales);
+                ctx.status(200);
+            }
+        } catch (SQLException e) {
+            ctx.result("Error retrieving sale: " + e.getMessage());
+            ctx.status(400);
+        }
+    }
+
+    @OpenApi(summary = "Get average sale price by postcode", operationId = "getAveragePrice", path = "/sales/average/{postcode}", methods = HttpMethod.GET, tags = {
+        "Sales"}, pathParams = {
+        @OpenApiParam(name = "postcode")
+    }, responses = {
+        @OpenApiResponse(status = "200"),
+        @OpenApiResponse(status = "400")
+    })
+    public void averagePrice(Context ctx, int postCode) {
+        try {
+            double averagePrice = homeSales.getAveragePrice(postCode);
+            ctx.result(Double.toString(averagePrice));
+        } catch (SQLException e) {
+            ctx.result("Error getting average: " + e.getMessage());
+            ctx.status(400);
+        }
+    }
+}
